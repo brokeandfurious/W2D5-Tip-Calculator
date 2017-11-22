523919a381a6d0b29796d9c09c4b765c6c7b6353
brokeandfurious <forumbomb@gmail.com>
2017-11-22 17:41:03 -0500

    Initial Commit

---
+++ W2D5-Tip-Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  W2D5-Tip-Calculator
+//
+//  Created by Murat Ekrem Kolcalar on 11/22/17.
+//  Copyright © 2017 murtilicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ W2D5-Tip-Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  W2D5-Tip-Calculator
+//
+//  Created by Murat Ekrem Kolcalar on 11/22/17.
+//  Copyright © 2017 murtilicious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
